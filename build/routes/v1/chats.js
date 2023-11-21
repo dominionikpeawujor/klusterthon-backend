206f66225d4045874a0c720975780bc3916a04ee
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-11-21 17:51:07 +0100

    Delete: Remove build

--- build/routes/v1/chats.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const chat_1 = require("../../handlers/chat");
-const chat = (0, express_1.Router)();
-const GptMethods = new chat_1.GptHandler();
-chat.post('/', GptMethods.chat);
-exports.default = chat;
--- build/services/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GptService = void 0;
-var chatgpt_1 = require("./chatgpt");
-Object.defineProperty(exports, "GptService", { enumerable: true, get: function () { return __importDefault(chatgpt_1).default; } });
