e565933de475e1ed00643d37ddfbb95b17e96edb
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-11-22 10:57:29 +0100

    Update: Rename endpoint from chat to diagnose

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import { Request, Response ,Router } from 'express';
 import chat from './v1/gpt';
 
 const routes: Router = Router();
-routes.use('/chat', chat);
+routes.use('/diagnose', chat);
 
 routes.get('/', (_req: Request, res: Response) => {
   res.send('Welcome!');
